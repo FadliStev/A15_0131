61f97baa3efd2bb04a924b6cef0e5bd9931916b1
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2025-01-22 20:14:13 +0700

    rename nama file

--- app/src/main/java/com/example/uas_pam_131/KebunBinatangApplication.kt
+++ app/src/main/java/com/example/uas_pam_131/KebunBinatangApplication.kt
@@ -3,7 +3,7 @@ package com.example.uas_pam_131
 import android.app.Application
 import com.example.uas_pam_131.di.PetugasContainer
 
-class PetugasApplication: Application() {
+class KebunBinatangApplication: Application() {
 
     lateinit var container: PetugasContainer
     override fun onCreate() {
