f08e89c26d7d8886b8b77acde8738844c6cb9db2
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2025-01-22 19:25:15 +0700

    membuat tabel petugas

---
+++ app/src/main/java/com/example/uas_pam_131/model/Petugas.kt
@@ -0,0 +1,33 @@
+package com.example.uas_pam_131.model
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Entity(
+    tableName = "petugas",
+    indices = [Index(value = ["nama_petugas"], unique = true)]
+)
+@Serializable
+data class Petugas(
+    @PrimaryKey(autoGenerate = true)
+    val id_petugas: Int = 0,
+    val nama_petuags: String,
+    val jabatan: String
+)
+
+@Serializable
+data class PetugasResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Petugas>
+
+)
+
+@Serializable
+data class PetugasResponseDetail(
+    val status: Boolean,
+    val message: String,
+    val data: Petugas
+)
